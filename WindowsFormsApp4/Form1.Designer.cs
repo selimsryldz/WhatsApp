230b4a4e7a2191d6c3cf6b9e09c7302e19adcf81
Selim Sarıyıldız <mselim03@hotmail.com>
2019-07-02 17:31:38 +0300

    Yeni

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yolla = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.text = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.profileImg = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.individuals = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.profileImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yolla
+            // 
+            this.yolla.Location = new System.Drawing.Point(738, 309);
+            this.yolla.Name = "yolla";
+            this.yolla.Size = new System.Drawing.Size(86, 57);
+            this.yolla.TabIndex = 0;
+            this.yolla.Text = "Yolla";
+            this.yolla.UseVisualStyleBackColor = true;
+            this.yolla.Click += new System.EventHandler(this.yolla_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(233, 326);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(462, 68);
+            this.textBox.TabIndex = 3;
+            this.textBox.Text = "";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.text.Location = new System.Drawing.Point(346, 12);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(59, 24);
+            this.text.TabIndex = 4;
+            this.text.Text = "selim";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(738, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "düzenle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // profileImg
+            // 
+            this.profileImg.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.profileImg.Location = new System.Drawing.Point(242, 12);
+            this.profileImg.Name = "profileImg";
+            this.profileImg.Size = new System.Drawing.Size(60, 62);
+            this.profileImg.TabIndex = 6;
+            this.profileImg.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(233, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(462, 196);
+            this.listBox1.TabIndex = 8;
+            // 
+            // individuals
+            // 
+            this.individuals.FormattingEnabled = true;
+            this.individuals.ItemHeight = 16;
+            this.individuals.Location = new System.Drawing.Point(12, 21);
+            this.individuals.Name = "individuals";
+            this.individuals.Size = new System.Drawing.Size(193, 356);
+            this.individuals.TabIndex = 9;
+            this.individuals.Click += new System.EventHandler(this.individuals_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 413);
+            this.Controls.Add(this.individuals);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.profileImg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.yolla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.profileImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yolla;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox profileImg;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox individuals;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,152 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver = new ChromeDriver();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            driver.Navigate().GoToUrl("https://web.whatsapp.com/");
+
+
+
+        }
+
+        private void yolla_Click(object sender, EventArgs e)
+        {
+            driver.FindElement(By.ClassName("_35EW6")).Click();
+            textBox.Clear();
+        }
+
+
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            //var element = driver.FindElement(By.ClassName("_2S1VP"));
+            //element.Clear();
+            //element.SendKeys(textBox.Text);
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var element = driver.FindElement(By.ClassName("_2S1VP"));
+            element.SendKeys(e.KeyChar.ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            individuals.Items.Clear();
+
+
+
+            //sohbetteki isimleri sıralayıp sol tarafa yazdırıyor
+            var individuallist = driver.FindElements(By.ClassName("_25Ooe")).ToList();
+            var selim = individuallist[individuallist.Count - 1];
+            List<IWebElement> yedekList = new List<IWebElement>();
+            var sayac = individuallist.Count - 1;
+            yedekList.Add(individuallist[0]);
+            for (int i = 1; i < individuallist.Count - 1; i++)
+            {
+                yedekList.Add(individuallist[sayac]);
+                sayac--;
+            }
+            foreach (var item in yedekList)
+            {
+
+                individuals.Items.Add(item.Text);
+            }
+
+
+
+        }
+
+        private void individuals_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            //sohbet isimlerine sıralı bir şekilde basılmasını sağlıyor
+            var individuallist = driver.FindElements(By.ClassName("_25Ooe")).ToList();
+            var selim = individuallist[individuallist.Count - 1];
+            List<IWebElement> yedekList = new List<IWebElement>();
+            var sayac = individuallist.Count - 1;
+            yedekList.Add(individuallist[0]);
+            for (int i = 1; i < individuallist.Count - 1; i++)
+            {
+                yedekList.Add(individuallist[sayac]);
+                sayac--;
+            }
+            var tikla = yedekList[individuals.SelectedIndex];
+            tikla.Click();
+
+
+
+
+
+            //sohbetin başlığını gösteriyor
+            var baslik = driver.FindElement(By.ClassName("_3XrHh"));
+            text.Text = baslik.Text;
+
+            System.Threading.Thread.Sleep(2000);
+
+            //mesajları alıp listboxa basıyor
+            var messagelist = driver.FindElements(By.ClassName("vW7d1")).ToList();
+
+            foreach (var item in messagelist)
+            {
+                var melo = item.FindElement(By.TagName("div"));
+                var selo = melo.GetAttribute("class");
+                var a = selo.Contains("message-out");
+                var b = selo.Contains("message-in");
+                if (a)
+                {
+                    listBox1.Items.Add("--------------------");
+                    listBox1.Items.Add(item.Text);
+                    listBox1.Items.Add("");
+
+                }
+                if(b)
+                {
+                    listBox1.Items.Add(item.Text);
+                    listBox1.Items.Add("");
+                }
+            }
+
+           
+            
+              
+
+
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                driver.FindElement(By.ClassName("_35EW6")).Click();
+                textBox.Clear();
+            }
+        }
+
+      
+    }
+}
